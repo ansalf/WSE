35346f581d93f4c8bf3672ff18d1a78d0f53c225
NovanAndreans <novanandre41@gmail.com>
2024-07-05 15:39:13 +0700

    Roling, Master Users & Setting Permissions

--- resources/views/Admin/Layouts/Components/sidebar.blade.php
+++ resources/views/Admin/Layouts/Components/sidebar.blade.php
@@ -17,42 +17,76 @@
       <ul class="sidebar-menu">        
         <button class="btn btn-primary" onclick="console.log({{$menus}})">tekan</button>
         @foreach ($menus as $item)
-        @if (count($item->children) == 0)
+        @php
+            $hasParentViewFeature = false;
+            // Periksa jika `features` adalah array dan cari `featslug` dengan nilai 'view'
+            if (count($item->features) > 0) {
+                foreach ($item->features as $feature) {
+                    if ($feature['featslug'] == 'view') {
+                      foreach ($feature->permissions as $permission) {
+                        if ($permission->permisfeatid == $feature->id) {
+                          $hasParentViewFeature = $permission->hasaccess;
+                          break;
+                        }
+                      }
+                    }
+                }
+            }
+        @endphp
+        @if (count($item->children) == 0 && $hasParentViewFeature)
             <li class="{{ activeMenu($item->menunm) }}"><a class="nav-link" href="{{ route($item->menuroute) }}"><i class="fas fa-fire"></i><span>{{$item->menunm}}</span></a></li>
         @else
+        @php
+            $hasParentViewFeature = false;
+            // Periksa jika `features` adalah array dan cari `featslug` dengan nilai 'view'
+            if (count($item->features) > 0) {
+                foreach ($item->features as $feature) {
+                    if ($feature['featslug'] == 'view') {
+                      foreach ($feature->permissions as $permission) {
+                        if ($permission->permisfeatid == $feature->id) {
+                          $hasParentViewFeature = $permission->hasaccess;
+                          break;
+                        }
+                      }
+                    }
+                }
+            }
+        @endphp
+        @if ($hasParentViewFeature)
         <li class="menu-header">{{$item->menunm}}</li>
-            <li class="{{ activeMenu($item->menunm) }} dropdown">
-              <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>{{$item->menunm}}</span></a>
-              <ul class="dropdown-menu">
-                @if (!empty($item->children) && count($item->children) > 0)
-                @foreach ($item->children as $child)
-                @php
-                    $hasViewFeature = false;
-                    // Periksa jika `features` adalah array dan cari `featslug` dengan nilai 'view'
-                    if (count($child->features) > 0) {
-                        foreach ($child->features as $feature) {
-                            if ($feature['featslug'] == 'view') {
-                              foreach ($feature->permissions as $permission) {
-                                if ($permission->permisfeatid == $feature->id) {
-                                  $hasViewFeature = $permission->hasaccess;
-                                  break;
-                                }
-                              }
+        <li class="{{ activeMenu($item->menunm) }} dropdown">
+          <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>{{$item->menunm}}</span></a>
+          <ul class="dropdown-menu">
+            @if (!empty($item->children) && count($item->children) > 0)
+            @foreach ($item->children as $child)
+            @php
+                $hasViewFeature = false;
+                // Periksa jika `features` adalah array dan cari `featslug` dengan nilai 'view'
+                if (count($child->features) > 0) {
+                    foreach ($child->features as $feature) {
+                        if ($feature['featslug'] == 'view') {
+                          foreach ($feature->permissions as $permission) {
+                            if ($permission->permisfeatid == $feature->id) {
+                              $hasViewFeature = $permission->hasaccess;
+                              break;
                             }
+                          }
                         }
                     }
-                @endphp
-                @if ($hasViewFeature)
-                    <li class="{{ activeMenu($child->menunm) }}">
-                        <a class="nav-link" href="{{ route($child->menuroute) }}">
-                            {{ $child->menunm }}
-                        </a>
-                    </li>
-                @endif
-            @endforeach
-                @endif
-              </ul>
-            </li>
+                }
+            @endphp
+            @if ($hasViewFeature)
+                <li class="{{ activeMenu($child->menunm) }}">
+                    <a class="nav-link" href="{{ route($child->menuroute) }}">
+                        {{ $child->menunm }}
+                    </a>
+                </li>
+            @endif
+        @endforeach
+            @endif
+          </ul>
+        </li>
+        @endif
         @endif
         @endforeach
       </ul>    
